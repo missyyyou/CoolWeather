cfec9548333557d6f1e42991f8e0d3fbe5675739
missyyyou <yysha-94-03@foxmail.com>
2017-08-03 07:17:04 +0800

    delete query class

--- app/src/main/java/com/apress/gerber/coolweather/model/http/QueryCallback.java
+++
@@ -1,9 +0,0 @@
-package com.apress.gerber.coolweather.model.http;
-
-import com.apress.gerber.coolweather.model.bean.WeatherInfo;
-
-public interface QueryCallback {
-    void onSuccess(WeatherInfo weatherInfo);
-
-    void onFail(String errMsg);
-}
--- app/src/main/java/com/apress/gerber/coolweather/model/http/QueryType.java
+++
@@ -1,5 +0,0 @@
-package com.apress.gerber.coolweather.model.http;
-
-public enum QueryType {
-    WEATHER_CODE, COUNTY_CODE
-}
